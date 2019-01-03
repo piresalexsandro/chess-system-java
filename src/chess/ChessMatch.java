7e19c8b85ff439678dfd3311e055039eae8325c0
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-01-03 14:47:46 -0200

    Possible move os a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -42,7 +42,10 @@ public class ChessMatch {
 	
 	public void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)){
-			throw new ChessException("There is no piece on source position");
+			throw new ChessException("There is not piece on source position");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is not possible moves for the chosen piece");
 		}
 	}
 	
